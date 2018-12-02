ad41bbd672fd8dc708699774633d8ac3160e8e06
Patrick Rivera <patrick.rivera@instacart.com>
2018-12-01 17:40:02 -0800

    Added todos-api project

---
+++ todos-api/db/schema.rb
@@ -0,0 +1,27 @@
+ActiveRecord::Schema.define(version: 20181202012903) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.boolean "done"
+    t.integer "todo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_id"], name: "index_items_on_todo_id"
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.string "created_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
