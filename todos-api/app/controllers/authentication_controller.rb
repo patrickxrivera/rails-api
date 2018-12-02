5f6cef42c03bd42ae0d69638d68efdf0219abb8a
Patrick Rivera <patrick.rivera@instacart.com>
2018-12-02 13:21:46 -0800

    WIP authentication

---
+++ todos-api/app/controllers/authentication_controller.rb
@@ -0,0 +1,15 @@
+class AuthenticationController < ApplicationController
+    skip_before_action :authorize_request, only: :authenticate
+
+    # return auth token once user is authenticated
+    def authenticate
+        auth_token = AuthenticateUser.new(email: auth_params[:email], password: auth_params[:password]).call
+        json_response(auth_token: auth_token)
+    end
+
+    private
+
+    def auth_params
+        params.permit(:email, :password)
+    end
+end
