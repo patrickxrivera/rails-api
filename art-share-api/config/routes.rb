932819e008257b07c51026d5bed002ac733812a8
Patrick Rivera <patrick.rivera@instacart.com>
2018-12-02 18:54:25 -0800

    WIP user controller v1

--- art-share-api/config/routes.rb
+++ art-share-api/config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  resources :artwork_shares
-  resources :artworks
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create, :destroy, :index, :show, :update]
 end
